f20a0f9dfc14e89be39123abaa5cda4d7687e4c6
joa242312 <joaogabrielss2006@gmail.com>
2025-04-23 10:13:08 -0300

    melorar anotação aula 08

---
+++ WTml/aula 0/script.js
@@ -0,0 +1,23 @@
+ //Clonar: Git clone -->
+ //Adicionar: Git ADD (só um) -->
+ //Git ADD .(todos)
+ //Comentario: Git comit -m " " -->
+//Enviar: Gti Push -->
+
+ // variaveis javascript -->
+ // Const -->
+// não pode modificar -->
+// Var -->
+// let -->
+// = (Atriduir)  -->
+ //== (conparar -valor)  -->
+//=== (Conparar - valor e tipo)  -->
+//& (e - um coisa e outra)  -->
+//|| (ou - um coisa ou outra)  -->
+//!= (-valor)  -->
+ //!== (-valor e tipo)   -->
+
+ //tipo dferente --> 
+ // <div class="w-full"></div> -->
+ //<div class="w-full bg-black h-secrem"></div> -->
+ 
